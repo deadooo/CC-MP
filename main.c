9bfec684b0b9d963333694e84b0907e3644e535e
Ken <155579723+deadooo@users.noreply.github.com>
2024-03-29 17:09:18 +0000

    SAev

--- main.c
+++ main.c
@@ -6,6 +6,7 @@
 #define playerO 2
 
 void dispBoard(int F3[][6]);
+int checkS(int S[][6][2], int C[][2], int inp_row, int inp_col);
 
 int main() {
   int good, over, next;
@@ -15,12 +16,14 @@ int main() {
 
   char F[6][6]; // Display Board
 
-  int S[4][6] = {
-      {(1, 1), (1, 3), (2, 2), (3, 1), (3, 3)},
-      {(4, 4), (4, 6), (5, 5), (6, 4), (6, 6)},
-      {(1, 5), (2, 4), (2, 5), (2, 6), (3, 5)},
-      {(4, 1), (4, 3), (5, 1), (5, 3), (6, 1), (6, 3)}
-}; // {{x, y}, {x, y}};
+  int S[4][6][2] = {
+      {{1, 1}, {1, 3}, {2, 2}, {3, 1}, {3, 3}},
+      {{4, 4}, {4, 6}, {5, 5}, {6, 4}, {6, 6}},
+      {{1, 5}, {2, 4}, {2, 5}, {2, 6}, {3, 5}},
+      {{4, 1}, {4, 3}, {5, 1}, {5, 3}, {6, 1}, {6, 3}}}; // {{x, y}, {x, y}};
+
+  printf("(%d, %d)\n", S[0][0][0], S[0][0][1]);
+
   int C[2][2]; // Player X/O Valid Moves
 
   int F1[6][6] = {0}, F2[6][6] = {0}, F3[6][6] = {0}, C1[MAX][2] = {0},
@@ -92,17 +95,16 @@ int main() {
     // Check if in S Board
 
     // Storing Values in C1 & C2 (current scores)
+    int svalue = checkS(S, C, row, col);
     int index1 = 0, indexC1 = 0, index2 = 0, indexC2 = 0;
-    if (F3[row][col] != 0 && player == playerX) {
-      C1[index1][indexC1] = c;
+    if (F3[row][col] != 0 && player == playerX && svalue == 1) {
       index1++; // comparison for scores for both players
       if (index1 > 36)
         indexC1++;
       if (indexC1 > 2)
         indexC1 = 0;
     }
-    if (F3[row][col] != 0 && player == playerO) {
-      C2[index2][indexC2] = c;
+    if (F3[row][col] != 0 && player == playerO && svalue == 1) {
       index2++;
       if (index2 > 36)
         indexC2++;
@@ -118,6 +120,13 @@ int main() {
     //   printf("Tie");
     // }
   } while (!over);
+}
+// comparing scores
+if (index1 > index2) {
+  printf("Player 1 wins");
+}
+if (index2 > index1) {
+  printf("Player 2 wins");
 }
 
 void dispBoard(int F3[][6]) {
@@ -154,86 +163,83 @@ void dispBoard(int F3[][6]) {
   }
 }
 
-int checkS(int S[][2], int C[][2], int inp_row, int inp_col) {
+int checkS(int S[][6][2], int C[][2], int inp_row, int inp_col) {
 
-  int row, col;
-  {1, 1}, {1, 3}, {1, 4},
-      - - - - - -
-
+  int row, col, f; // f determines the x and y in (x, y)
   {
-    {1, 1}, {1, 3}, {}
-  }
-  for (row = 0; row < 21, row++) {
-    for (col = 0; col < 2; col++) {
-      if (S[row][col] ==)
-    }
-  }
-  {}
-}
 
-/*Checking S
-for (i = 0; i < n; i++){
-
-  while ()
-}
-
-*/
-/* Linear Search
-      for (i =0; i < n; i++)
-      {
-        for (j = 0; j < 2; j++)
-          if (S[i][j] == x)
-      }
-
-      for (i = 0; i < 2; i++){
-        num = i;
-        if(S[i][num] == x){
-          num++;
-          if(S[i][num]){
-            printf("Found");
-          }else{
-            num--;
-          }
+    for (row = 0; row < 4; row++) {
+      for (col = 0; col < 6; col++) {
+        if (S[row][col][0] == inp_row && S[row][col][1] == inp_col) {
+          return 1;
         }
       }
-
-
-
-        */
-// whole board
-//  {(1,1), (1,2), (1,3), (1,4), (1,5), (1,6)},
-//  {(2,1), (2,2), (2,3), (2,4), (2,5), (2,6)},
-//  {(3,1), (3,2), (3,3), (3,4), (3,5), (3,6)},
-//  {(4,1), (4,2), (4,3), (4,4), (4,5), (4,6)},
-//  {(5,1), (5,2), (5,3), (5,4), (5,5), (5,6)},
-//  {(6,1), (6,2), (6,3), (6,4), (6,5), (6,6)};
-
-// correct values
-
-/*
-{
-{(1,1), (1, 3), (2, 2), (3, 1), (3, 3)},
-  {(4, 4), (4, 6), (5, 5), (6, 4), (6,6)},
-  {(1, 5), (2, 4), (2, 5), (2, 6), (3, 5)},
-  {(4, 1), (4, 3), (5, 1), (5, 3), (6, 1), (6, 3)}
-}
-*/
-
-// FINISHED(USED)
-/* for C1 && C2
-int index1 = 0, C1_2 = 0, index2 = 0, C2_2 = 0;
-  if (F3[row][col] != NULL && player == playerX){
-    C1[index1][C1_2] == c;
-    C1_2++;
-    index1++;
-    if (C1_2 > 2)
-      C1_2 = 0;
     }
-  if (F3[row][col] != NULL && player == playerO){
-    C2[index2][C2_2] == c;
-    index2++;
-    C2_2++;
-    if (C2_2 > 2)
-      C2_2 = 0;
   }
-*/
+
+  /*Checking S
+  for (i = 0; i < n; i++){
+
+    while ()
+  }
+
+  */
+  /* Linear Search
+        for (i =0; i < n; i++)
+        {
+          for (j = 0; j < 2; j++)
+            if (S[i][j] == x)
+        }
+
+        for (i = 0; i < 2; i++){
+          num = i;
+          if(S[i][num] == x){
+            num++;
+            if(S[i][num]){
+              printf("Found");
+            }else{
+              num--;
+            }
+          }
+        }
+
+
+
+          */
+  // whole board
+  //  {(1,1), (1,2), (1,3), (1,4), (1,5), (1,6)},
+  //  {(2,1), (2,2), (2,3), (2,4), (2,5), (2,6)},
+  //  {(3,1), (3,2), (3,3), (3,4), (3,5), (3,6)},
+  //  {(4,1), (4,2), (4,3), (4,4), (4,5), (4,6)},
+  //  {(5,1), (5,2), (5,3), (5,4), (5,5), (5,6)},
+  //  {(6,1), (6,2), (6,3), (6,4), (6,5), (6,6)};
+
+  // correct values
+
+  /*
+  {
+  {(1,1), (1, 3), (2, 2), (3, 1), (3, 3)},
+    {(4, 4), (4, 6), (5, 5), (6, 4), (6,6)},
+    {(1, 5), (2, 4), (2, 5), (2, 6), (3, 5)},
+    {(4, 1), (4, 3), (5, 1), (5, 3), (6, 1), (6, 3)}
+  }
+  */
+
+  // FINISHED(USED)
+  /* for C1 && C2
+  int index1 = 0, C1_2 = 0, index2 = 0, C2_2 = 0;
+    if (F3[row][col] != NULL && player == playerX){
+      C1[index1][C1_2] == c;
+      C1_2++;
+      index1++;
+      if (C1_2 > 2)
+        C1_2 = 0;
+      }
+    if (F3[row][col] != NULL && player == playerO){
+      C2[index2][C2_2] == c;
+      index2++;
+      C2_2++;
+      if (C2_2 > 2)
+        C2_2 = 0;
+    }
+  */
