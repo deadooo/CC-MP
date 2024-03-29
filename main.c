c90382dff65722fa4868ce358697a96343afa006
Ken <155579723+deadooo@users.noreply.github.com>
2024-03-29 14:32:11 +0000

    save

--- main.c
+++ main.c
@@ -6,23 +6,59 @@
 int main() {
   int good, over, next;
   int a, b, c, d;
-  char F[6][6]; //  x  y
-  int S[MAX][2] = {{1, 1}, {1, 3}};
-  int C[2][2];
-  int F1[MAX], F2, C1, C2; // Variables
-  int x, y;
+  int x, y; // Player Coords
 
-  good = next = FALSE;
-  over = 
-  do{
+  char F[6][6]; // Display Board
+
+  int S[MAX][2] = {{1, 1}, {1, 3}}; // {{x, y}, {x, y}};
+  int C[2][2];                      // Player X/O Valid Moves
+
+  int F1[6][6] = {0}, F2[6][6] = {0}, F3[6][6], C1[MAX][2] = {0},
+      C2[MAX][2] = {0}; // Variables
+
+  /*
+
+  F3
+  for (i = 0; i < n; i++){
+    for (j = 0; j < n; j++){
+      if (F[i][j] == F[x][y]){
+        F[i][j]
+
+
+      }
+    }
+  }
+
+  x y
+  1 3
+  F3[y][x] = O;
+
+  F3[x][y] = null;
+
+  if (F3[x][y] == null)
+
+  F3
+  board
+
+  x y  - -
+  0 0, 0 1, 0 2
+
+
+  */
+  good = next = over = FALSE;
+  do {
     printf("Enter row: ");
     scanf("%d", &y);
-  }while (y < 0 || y > 6);
+  } while (y < 0 || y > 6);
 
-  do{
+  do {
     printf("\nEnter col: ");
     scanf("%d", &x);
-  }while (x < 0 || x > 6);
+  } while (x < 0 || x > 6);
+
+  // check if F[y][x] is empty
+  // if F3[y][x] is empty = null, then put O or X
+  //
 
   return 0;
 }
@@ -58,10 +94,11 @@ int main() {
 
 // correct values
 
+/*
 {
-  {(1, 1), (1, 3), (2, 2), (3, 1), (3, 3)} {(4, 4), (4, 6), (5, 5), (6, 4),
-                                            (6, 6)},
-      {(1, 5), (2, 4), (2, 5), (2, 6), (3, 5)}, {
-    (4, 1), (4, 3), (5, 1), (5, 3), (6, 1), (6, 3)
-  }
+{(1,1), (1, 3), (2, 2), (3, 1), (3, 3)},
+  {(4, 4), (4, 6), (5, 5), (6, 4), (6,6)},
+  {(1, 5), (2, 4), (2, 5), (2, 6), (3, 5)},
+  {(4, 1), (4, 3), (5, 1), (5, 3), (6, 1), (6, 3)}
 }
+*/
