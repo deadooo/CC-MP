98cc2266a1cf4e68eecdf8b04a593f987804e41d
Ken <155579723+deadooo@users.noreply.github.com>
2024-03-30 13:40:12 +0000

    Testing

--- .ccls-cache/@home@runner@CC-MP/main.c
+++ .ccls-cache/@home@runner@CC-MP/main.c
@@ -175,13 +175,12 @@ int gameOver(int F3[][6], int C1[][2], int C2[][2]) {
   int count = 0;
   for (int i = 0; i < 6; i++) {
     for (int j = 0; j < 6; j++) {
-      if (F3[i][j] > 0)
-      {
-          count++;
+      if (F3[i][j] > 0) {
+        count++;
       }
     }
   }
-
+  printf("\nCount: %d\n", count);
   if (count == 36) {
     for (int i = 0; i < 36; i++) {
       if (C1[i][0] == 1 && C1[i][1] == 1) { // (1,1)
